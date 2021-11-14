93905dd2567e88d2a846aa38762814379ed17da5
Banto Balazs <balazs.banto@agroninja.com>
2021-11-14 11:22:05 +0100

    Initial commit

---
+++ app/src/main/java/com/blade/cameraclient/fragment/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.blade.cameraclient.fragment
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.blade.cameraclient.R
+import com.blade.cameraclient.databinding.HomeFragmentBinding
+
+class HomeFragment : Fragment() {
+
+    lateinit var binding: HomeFragmentBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = HomeFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.startCameraButton.setOnClickListener {
+            findNavController().navigate(R.id.action_home_fragment_to_permissions_fragment)
+        }
+    }
+}
